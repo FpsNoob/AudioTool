2b3ae31ea3c7b2c44ce820780125d2bc43f0ceac
FpsNoob <18850128056@163.com>
2020-07-30 18:27:09 +0800

    add find_words, can find the words we wanted in the sentence.
    (cherry picked from commit 18dc2210be7e75d15f36550ee49dfb623cdf8631)

--- find_words.py
+++ find_words.py
@@ -11,11 +11,21 @@ def get_words(filepath):
     return word_list
 
 def find_word(folder_path, word_list):
+    '''
+    find the sentence in the audio dataset, which including words we wanted
+    Args:
+        folder_path: folder contains the audio's label
+        word_list: the list of words you wanted
+    Return:
+        the path to the audio
+        aud
+    '''
     need_to_save = []
     search_path = os.path.join(folder_path, '*', '*.txt')
     for txt_path in gfile.Glob(search_path):
         txt_file = open(txt_path)
         for sentence in txt_file:
+            print(sentence)
             for word in word_list:
                 if re.search(word, sentence, re.IGNORECASE):
                     need_to_save.append(txt_path)
@@ -28,15 +38,18 @@ def find_word(folder_path, word_list):
     return need_to_save
 
 def copy_file():
-    ''' copy wav file to the folder we set, using the path in the label file
+    ''' copy the file to the path we set, which is uses the label saved in find_word function
+        Agrs:
+         file name
+
     '''
 
 if __name__ == '__main__':
     word_list = get_words(r'words.txt')
     for word in word_list:
         print(word)
-    need_to_save = find_word(r'D:\chrome\LibriSpeech ASR corpus', word_list)
-    f = open(r'D:\chrome\temp.txt', 'a')
+    need_to_save = find_word(r'D:\chrome\Free_ST', word_list)
+    f = open(r'D:\chrome\path_1.txt', 'a')
     for sentence in need_to_save:
         f.write(sentence)
         f.write('\n')
